4864368e59db0ec401c545e2b332a3295f2ad210
aleramirezsj <aleramirezsj@gmail.com>
2024-08-19 17:36:19 -0300

    Agregar archivos de proyecto.

---
+++ InstitutoApp/Interfaces/IServiceGeneric.cs
@@ -0,0 +1,12 @@
+﻿
+namespace InstitutoApp.Interfaces
+{
+    public interface IGenericService<T> where T : class
+    {
+        public Task<List<T>?> GetAllAsync();
+        public Task<T?> GetByIdAsync(int id);
+        public Task<T?> AddAsync(T? entity);
+        public Task UpdateAsync(T? entity);
+        public Task DeleteAsync(int id);
+    }
+}
---
+++ InstitutoApp/Views/Commons/CarrerasView.xaml.cs
@@ -0,0 +1,9 @@
+namespace InstitutoApp.Views;
+
+public partial class CarrerasView : ContentPage
+{
+	public CarrerasView()
+	{
+		InitializeComponent();
+	}
+}
